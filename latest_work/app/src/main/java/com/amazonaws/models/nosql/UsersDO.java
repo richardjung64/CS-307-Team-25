162cb16f607537017c3fdba491bd593af032c553
AnqiYu2333 <yu208@purdue.edu>
2017-04-04 00:15:39 -0400

    add folder content to the latest_work

---
+++ latest_work/app/src/main/java/com/amazonaws/models/nosql/UsersDO.java
@@ -0,0 +1,111 @@
+package com.amazonaws.models.nosql;
+
+import com.amazonaws.mobile.user.IdentityManager;
+import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBAttribute;
+import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBHashKey;
+import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBIndexHashKey;
+import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBIndexRangeKey;
+import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBRangeKey;
+import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBTable;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+@DynamoDBTable(tableName = "stylin-mobilehub-1048106400-Users")
+
+public class UsersDO {
+    private String _userId;
+    private Double _userAge;
+    private String _userDescription;
+    private String _userGender;
+    private String _userName;
+    private String _userPhoto;
+    private List<String> _userPosts;
+    private byte[] _userPrivacy;
+    private Set<String> _usersFollowers;
+    private Set<String> _usersFollowing;
+
+    @DynamoDBHashKey(attributeName = "userId")
+    @DynamoDBAttribute(attributeName = "userId")
+    public String getUserId() {
+        return _userId;
+    }
+
+    public void setUserId() {
+        this._userId = "usr101";
+    }
+    @DynamoDBAttribute(attributeName = "User_Age")
+    public Double getUserAge() {
+        return _userAge;
+    }
+
+    public void setUserAge(final Double _userAge) {
+        this._userAge = _userAge;
+    }
+    @DynamoDBAttribute(attributeName = "User_Description")
+    public String getUserDescription() {
+        return _userDescription;
+    }
+
+    public void setUserDescription(final String _userDescription) {
+        this._userDescription = _userDescription;
+    }
+    @DynamoDBAttribute(attributeName = "User_Gender")
+    public String getUserGender() {
+        return _userGender;
+    }
+
+    public void setUserGender(final String _userGender) {
+        this._userGender = _userGender;
+    }
+    @DynamoDBAttribute(attributeName = "User_Name")
+    public String getUserName() {
+        return _userName;
+    }
+
+    public void setUserName(final String _userName) {
+        this._userName = _userName;
+    }
+    @DynamoDBAttribute(attributeName = "User_Photo")
+    public String getUserPhoto() {
+        return _userPhoto;
+    }
+
+    public void setUserPhoto(final String _userPhoto) {
+        this._userPhoto = _userPhoto;
+    }
+    @DynamoDBAttribute(attributeName = "User_Posts")
+    public List<String> getUserPosts() {
+        return _userPosts;
+    }
+
+    public void setUserPosts(final List<String> _userPosts) {
+        this._userPosts = _userPosts;
+    }
+    @DynamoDBAttribute(attributeName = "User_Privacy")
+    public byte[] getUserPrivacy() {
+        return _userPrivacy;
+    }
+
+    public void setUserPrivacy(final byte[] _userPrivacy) {
+        this._userPrivacy = _userPrivacy;
+    }
+    @DynamoDBAttribute(attributeName = "Users_Followers")
+    public Set<String> getUsersFollowers() {
+        return _usersFollowers;
+    }
+
+    public void setUsersFollowers(final Set<String> _usersFollowers) {
+        this._usersFollowers = _usersFollowers;
+    }
+    @DynamoDBAttribute(attributeName = "Users_Following")
+    public Set<String> getUsersFollowing() {
+        return _usersFollowing;
+    }
+
+    public void setUsersFollowing(final Set<String> _usersFollowing) {
+        this._usersFollowing = _usersFollowing;
+    }
+
+}
---
+++ latest_work/app/src/main/java/com/styln/HomeActivity.java
@@ -0,0 +1,147 @@
+package com.styln;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.amazonaws.mobile.user.signin.FacebookSignInProvider;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class HomeActivity extends AppCompatActivity {
+
+    private static final String LOG_TAG = HomeActivity.class.getSimpleName();
+    private ImageView imageLike;
+    private TextView textLikes;
+    private ImageView profilePic;
+    private Button follow;
+
+    static boolean liked = false;
+    static int numLikes = 0;
+    static boolean followed = false;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        imageLike = (ImageView)findViewById(R.id.like);
+        textLikes = (TextView)findViewById(R.id.numLikes);
+        follow = (Button)findViewById(R.id.follow);
+
+        profilePic = (ImageView)findViewById(R.id.profilePicture);
+        String address = FacebookSignInProvider.userImageUrl;
+        new LoadURLImage(address, profilePic).execute();
+
+
+        if(liked){
+            imageLike.setImageResource(R.drawable.main_like_1);
+            textLikes.setText(numLikes+" likes");
+        } else {
+            imageLike.setImageResource(R.drawable.main_like_0);
+            textLikes.setText(numLikes+" likes");
+        }
+        if(followed){
+            follow.setText("UNFOLLOW");
+            follow.setTextSize(10);
+    } else {
+            follow.setText("FOLLOW");
+            follow.setTextSize(12);
+    }
+
+    }
+
+
+
+    public void openHome(View view) {
+        Log.d(LOG_TAG, "Launching Main Activity...");
+        startActivity(new Intent(HomeActivity.this, HomeActivity.class)
+                .setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
+        // finish should always be called on the main thread.
+        finish();
+    }
+
+    public void openTrend(View view) {
+        Log.d(LOG_TAG, "Launching Trend Activity...");
+        startActivity(new Intent(HomeActivity.this, TrendActivity.class)
+                .setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
+        // finish should always be called on the main thread.
+        finish();
+    }
+
+    public void openPost(View view) {
+    }
+
+    public void openBrowse(View view) {
+    }
+
+    public void openProfile(View view) {
+        Log.d(LOG_TAG, "Launching Profile Activity...");
+        startActivity(new Intent(HomeActivity.this, ProfileActivity.class)
+                .setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
+        // finish should always be called on the main thread.
+        finish();
+    }
+
+    public void openSettings(View view) {
+    }
+
+    public void likePost(View view) {
+        if(liked == false){
+            liked = true;
+            numLikes++;
+            imageLike.setImageResource(R.drawable.main_like_1);
+            textLikes.setText(numLikes+" likes");
+        } else {
+            liked = false;
+            numLikes--;
+            imageLike.setImageResource(R.drawable.main_like_0);
+            textLikes.setText(numLikes+" likes");
+        }
+    }
+
+    public void addItem(View view) {
+        int id = view.getId();
+        if(id == R.id.Bitem1){
+            ProfileActivity.show1 = true;
+        }
+        if(id == R.id.Bitem2){
+            ProfileActivity.show2 = true;
+        }
+        Button a = (Button) findViewById(R.id.Bitem1);
+        Button b = (Button) findViewById(R.id.Bitem2);
+        a.setVisibility(View.INVISIBLE);
+        b.setVisibility(View.INVISIBLE);
+    }
+    public void addTo(View view) {
+        Button a = (Button) findViewById(R.id.Bitem1);
+        Button b = (Button) findViewById(R.id.Bitem2);
+       a.setVisibility(View.VISIBLE);
+        b.setVisibility(View.VISIBLE);
+    }
+
+    public void follow(View view) {
+        if(followed){
+            followed = false;
+            follow.setText("FOLLOW");
+            follow.setTextSize(12);
+        } else {
+            followed = true;
+            follow.setText("UNFOLLOW");
+            follow.setTextSize(10);
+        }
+    }
+
+
+}

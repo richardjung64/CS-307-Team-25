32a504e2f4f2299e094d9314448d334f623ae457
Tiger Lu <shalingyi@ShaLingyideMacBook-Pro.local>
2017-04-05 06:00:45 -0400

    minor

--- app/build/generated/source/r/debug/com/google/android/gms/R.java
+++ app/build/generated/source/r/debug/com/google/android/gms/R.java
@@ -70,7 +70,7 @@ public final class R {
         public static final int standard = 0x7f0c0045;
         public static final int text = 0x7f0c010a;
         public static final int text2 = 0x7f0c0108;
-        public static final int toolbar = 0x7f0c00b0;
+        public static final int toolbar = 0x7f0c00af;
         public static final int wide = 0x7f0c0046;
         public static final int wrap_content = 0x7f0c0024;
     }
